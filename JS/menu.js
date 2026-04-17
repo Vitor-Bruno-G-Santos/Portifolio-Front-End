9fbd5a88fba6f71ef92c80fd4fc15ffe186add91
Vitor-Bruno-G-Santos <vitor_saske@hotmail.com>
2026-04-17 11:02:47 -0400

    task 06
    Responsividade feita

--- JS/menu.js
+++ JS/menu.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
-    $('.mobile-btn').on('click', function(){
+    $('.mobile-button').on('click', function(){
         $('.mobile-menu').toggleClass('active');
         $(this).find('i').toggleClass('fa-bars fa-x');
     })
+})
+
+$(document).ready(function(){
+    $('.items-list').on('click', function(){
+        $('.mobile-menu').toggleClass('active');
+        $('.mobile-button').find('i').toggleClass('fa-x fa-bars')
+    })
+    
 })
